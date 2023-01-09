640a4f0d9dfac0dbdd77e5f94dd87e6997ee8efd
Aleksander <spas.aleksander.v@gmail.com>
2023-01-09 18:34:20 +0300

    Задание выволнено

--- Final-Project-Programming-/Задача 64/Program.cs
+++ Final-Project-Programming-/Задача 64/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+*/
+
+Console.WriteLine("Введите число N: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+
+void Natural(int num)
+{
+    Console.Write(num + " ");
+    num--;
+
+    if (num >= 1)
+    {
+        Natural(num);
+    }
+}
+
+Natural(num);
