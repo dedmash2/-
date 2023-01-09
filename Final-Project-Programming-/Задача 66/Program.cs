33fb6cd681dbbeb897ab04b51dc90556ea2863aa
Aleksander <spas.aleksander.v@gmail.com>
2023-01-09 19:27:03 +0300

    Создан шаблон конс. приложения

--- Final-Project-Programming-/Задача 66/Program.cs
+++ Final-Project-Programming-/Задача 66/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+*/
+
+Console.WriteLine("Введите число N(побольше): ");
+int N = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите число M(поменьше): ");
+int M = int.Parse(Console.ReadLine() ?? "");
+
+int NaturalSum(int N, int M)
+{
+    if (N == M)
+    {
+        Console.WriteLine(M);
+    }
+    int sum = M + NaturalSum(N, M + 1);
+    Console.WriteLine(sum);
+
+    return sum;
+}
+
+NaturalSum(N, M);
