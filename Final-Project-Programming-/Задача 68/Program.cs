41c832fe2afab64cf055aa720e3d2813f995f5c1
Aleksander <spas.aleksander.v@gmail.com>
2023-01-09 19:29:57 +0300

    Задание недоделано

--- Final-Project-Programming-/Задача 68/Program.cs
+++ Final-Project-Programming-/Задача 68/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+*/
+
+int Akkerman(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return A(n - 1, 1);
+    else
+        return A(n - 1, A(n, m - 1));
+}
